8f61149b0c32c9724208b8bbbf5a32de454ffb11
ActuallyAJL <zero3260@hotmail.com>
2022-02-22 14:07:06 -0600

    added readme, buttons, background color, and functions

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # personalWebsite
+
+
+This is the personal website for Alexander James Levy, @DrLeisure @ActuallyAJL. Take a look around and enjoy my projects, and then send me bitcoin if you enjoy them.
---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+const buttonOne = () => {
+    console.log ("button one pressed");
+}
+
+const buttonTwo = () => {
+    console.log ("button two pressed");
+}
+
+const buttonThree = () => {
+    console.log ("button three pressed");
+}
+
+const buttonOneElem = document.getElementById("button-one");
+const buttonTwoElem = document.getElementById("button-two");
+const buttonThreeElem = document.getElementById("button-three");
