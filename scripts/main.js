d036610a564eb0eb72bb3f04bd1f9c7e89cd9c26
ActuallyAJL <zero3260@hotmail.com>
2022-02-25 08:42:48 -0600

    attempted to add multiple test pages to website, unsuccessfully

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,19 @@
 const buttonOne = () => {
-    console.log ("button one pressed");
+    // console.log ("button one pressed");
+    textBox.innerText += "Button One Pressed!";
 }
 
 const buttonTwo = () => {
-    console.log ("button two pressed");
+    // console.log ("button two pressed");
+    textBox.innerText += "Button Two Pressed!";
 }
 
 const buttonThree = () => {
-    console.log ("button three pressed");
+    // console.log ("button three pressed");
+    textBox.innerText += "Button Three Pressed!";
 }
 
 const buttonOneElem = document.getElementById("button-one");
 const buttonTwoElem = document.getElementById("button-two");
 const buttonThreeElem = document.getElementById("button-three");
+const textBox = document.getElementById("text-output");
