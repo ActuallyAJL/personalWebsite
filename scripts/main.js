0198f113c676d5d86bc1cb6fe0582de107522066
ActuallyAJL <zero3260@hotmail.com>
2022-02-28 13:07:21 -0600

    added the beginnings of a daily journal to my page

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,19 @@
 const buttonOne = () => {
     // console.log ("button one pressed");
-    textBox.innerText += "Button One Pressed!";
+    textBox.innerHTML += "Button One Pressed! <br>";
 }
 
 const buttonTwo = () => {
     // console.log ("button two pressed");
-    textBox.innerText += "Button Two Pressed!";
+    textBox.innerHTML += "Button Two Pressed! <br>";
 }
 
 const buttonThree = () => {
     // console.log ("button three pressed");
-    textBox.innerText += "Button Three Pressed!";
+    textBox.innerHTML += "Button Three Pressed! <br>";
 }
 
-const buttonOneElem = document.getElementById("button-one");
-const buttonTwoElem = document.getElementById("button-two");
-const buttonThreeElem = document.getElementById("button-three");
-const textBox = document.getElementById("text-output");
+const buttonOneElem = document.querySelector("#button-one");
+const buttonTwoElem = document.querySelector("#button-two");
+const buttonThreeElem = document.querySelector("#button-three");
+const textBox = document.querySelector("#text-output");
